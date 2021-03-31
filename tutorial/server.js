5059be296cd8c584e17408bc313f4a4b7139c87d
HrudaiKoda <hrudai.mastermind@gmail.com>
2021-03-31 12:15:20 +0530

    Working app with authentication using passport package

--- tutorial/server.js
+++ tutorial/server.js
@@ -4,6 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+
+const passport = require("passport");
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 var fs = require('fs');
 var path = require('path');
 require('dotenv/config');
@@ -26,12 +31,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+    secret:"Ourlitllesecret",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/newdb",{ useNewUrlParser: true });
 
+mongoose.set("useCreateIndex",true);
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
+  entered_by: String,
   img:
   {
       data: Buffer,
@@ -40,44 +67,59 @@ const postSchema = new mongoose.Schema({
   }
 });
 
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-  });
-
 const Post = mongoose.model("Post", postSchema);
-const user = mongoose.model("user", userSchema);
 
-app.get("/register",function(req,res){
-    res.render("register");
-});
+
+
 
 app.get("/login",function(req,res){
     res.render("login");
 });
 
-app.post("/register",function(req,res){
-    var ma = req.body.Email;
-    var pas = req.body.password;
-    console.log(ma);
-    console.log(pas);
-    const user_1 = new user({
-        email: ma,
-        password: pas
-      });
-    
-    
-      user_1.save(function(err){
-        if (!err){
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/login",function(req,res){
+    const user = new User({
+        username:req.body.username,
+        password: req.body.password
+    });
+    req.login(user,function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            passport.authenticate("local");
             res.redirect("/");
         }
-      });
+    })
+});
 
+app.post("/register",function(req,res){
+    User.register({username:req.body.username},req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+          }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/");
+            })
+        }
+    })
 });
 
 
 
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+})
+
+
 app.get("/",function(req,res){
+    if(req.isAuthenticated()){
     
     Post.find({},function(err,file){
         if(err){
@@ -88,18 +130,29 @@ app.get("/",function(req,res){
         res.render("home" ,{posts:posts});
     }
     });
+    }
+    else{
+        res.redirect("/login");
+    }
     
 });
 
 
 app.get("/compose",function(req,res){
-    res.render("compose");
+    if(req.isAuthenticated()){
+    var a = req.user._id;
+    res.render("compose",{user:a});
+    }
+    else{
+        res.redirect("/login");
+    }
 })
 
 app.post("/compose",upload.single('image'),function(req,res){
 const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
+    entered_by: req.body.user,
     img: {
         data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
         contentType: 'image/png' ,
@@ -115,7 +168,7 @@ const post = new Post({
   });
 })
 
-app.get("/:id",function(req,res){
+app.get("/view/:id",function(req,res){
     
     const post_id = req.params.id;
     Post.findOne({_id:post_id},function(err,pos){
@@ -135,8 +188,6 @@ app.post("/editpost/:po_id",function(req,res){
     const id_1 = req.params.po_id
     var ti =  req.body.postTitle;
     var con = req.body.postBody;
-    //console.log(ti)
-    //console.log(con)
     Post.findByIdAndUpdate(id_1,{title:ti , content:con},function(err,fin){
         if(err){
             res.send(err);
@@ -150,3 +201,4 @@ app.post("/editpost/:po_id",function(req,res){
 app.listen(4000, function() {
     console.log("Server started on port 3000");
   });
+
